b803c790429fefb4bf024eb8d2366002d07291e2
Thomas ILLIET <contact@thomas-illiet.fr>
2018-03-07 22:58:31 +0100

    update

---
+++ PowerGraph/Cmdlet/Get-PGSubscribedSkus.cs
@@ -0,0 +1,31 @@
+﻿using System.Management.Automation;
+using PowerGraph.Model;
+
+namespace PowerGraph
+{
+    [OutputType(typeof(object))]
+    [Cmdlet(VerbsCommon.Get, "PGSubscribedSkus")]
+    public class Get_PGSubscribedSkus : Cmdlet
+    {
+
+        protected override void ProcessRecord()
+        {
+            var GetAPI = new Get_API();
+
+            SubscribedSkus UsersResult = GetAPI.ExecuteGet<SubscribedSkus>("v1.0", "subscribedSkus");
+            var Cache = UsersResult.value;
+            var NextLink = UsersResult.nextLink;
+
+            if (!System.String.IsNullOrEmpty(NextLink))
+            {
+                do
+                {
+                    SubscribedSkus UsersResultNext = GetAPI.ExecuteGet<SubscribedSkus>(NextLink);
+                    Cache.AddRange(UsersResultNext.value);
+                    NextLink = UsersResultNext.nextLink;
+                } while (NextLink != null);
+            }
+            WriteObject(Cache);
+        }
+    }
+}
---
+++ PowerGraph/Model/SubscribedSku.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+
+namespace PowerGraph.Model
+{
+    public class SubscribedSku
+    {
+        public String id;
+        public Int16 consumedUnits;
+        public String capabilityStatus;
+        public String skuIdn;
+        public String skuPartNumber;
+
+    }
+
+    public class SubscribedSkus
+    {
+        [JsonProperty("@odata.context")]
+        public string context;
+
+        [JsonProperty("@odata.nextLink")]
+        public string nextLink;
+
+        public List<SubscribedSku> value;
+    }
+}
